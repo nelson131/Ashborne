049a2dab778a4ada04e878ac373fc6280c53e11e
Dima <dmitrygorokhov10@gmail.com>
2025-06-25 11:32:25 +0300

    texturemanager and other fixes

---
+++ src/TextureManager.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "TextureManager.h"
+
+TextureManager::TextureManager(){
+
+}
+
+SDL_Texture *TextureManager::load(const char *pathToFIle, SDL_Renderer *renderer){
+    SDL_Surface *tempSurface = IMG_Load(pathToFIle);
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+    SDL_FreeSurface(tempSurface);
+    return texture;
+}
---
+++ src/TextureManager.h
@@ -0,0 +1,16 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+class TextureManager {
+
+    public:
+    TextureManager();
+    
+    SDL_Texture *load(const char *pathToFIle, SDL_Renderer *renderer);
+
+};
+
+#endif
